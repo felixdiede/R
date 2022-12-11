42ac6cb14537b8741fba6e58c683b60986627bb4
fdiederi <fdiederi@uni-muenster.de>
2022-12-11 14:21:50 +0100

    added Aufgabe 9.3

--- WS 2022/Aufgabe 9.3 (fitness) /script/Ex3.R
+++ WS 2022/Aufgabe 9.3 (fitness) /script/Ex3.R
@@ -11,4 +11,60 @@ fitness_number_of_participants <- length(unique(fitness$Id))
 fitness_participants <- list(unique(fitness$Id))
 
 # 2 Wer ist der aktivste Teilnehmer in Bezug auf Schrittzahl, Distanz, aktive Minuten und verbrannte Kalorien? 
+fitness_filter_df <- fitness[order(fitness$TotalSteps, fitness$TotalDistance, fitness$TotalActiveMinutes, fitness$Calories, decreasing = TRUE), ]
+fitness_most_active_participant <- fitness_filter_df$Id[1]
 
+# 3 Über welchen Zeitraum wurden die Daten gesammelt? 
+fitness_max_date <- as.Date(max(fitness$ActivityDate))
+fitness_min_date <- as.Date(min(fitness$ActivityDate))
+
+fitness_period <- fitness_max_date - fitness_min_date # 146 Tage 
+
+# 4 Welcher Tag war der durchschnittlich sportlichste in Bezug auf die aktiven Minuten? 
+fitness_aggregate <- aggregate(fitness$Id, by = list(unique(fitness$ActivityDate)), FUN = "sum")
+
+
+# 5 Wie lange ist der durchschnittliche Schritt der Teilnehmer? 
+fitness_average_step <- mean(fitness$TotalSteps)
+
+# Aufgabe 9.3 c) 
+fitness_histogramm_steps <- hist(fitness$TotalSteps, 
+                                xlab = "Anzahl der Schritte", 
+                                ylab = "Häufigkeit der Schritte")
+
+fitness_boxplot_steps <- boxplot(fitness$TotalSteps)
+
+par(mfrow = c(1,2))
+
+fitness_histogramm_distance <- hist(fitness$TotalDistance, 
+                                xlab = "Distanzen", 
+                                ylab = "Häufigkeit der Distanzen")
+
+fitness_boxplot_distance <- boxplot(fitness$TotalDistance)
+
+par(mfrow = c(1,2))
+
+
+#....
+
+# Aufgabe 9.3 d) 
+install.packages("ineq")
+library(ineq)
+
+Lc(fitness$TotalSteps, plot = TRUE) 
+Lc(fitness$TotalDistance, plot = TRUE) 
+Lc(fitness$TotalActiveMinutes, plot = TRUE) 
+Lc(fitness$SedentaryMinutes, plot = TRUE) 
+Lc(fitness$Calories, plot = TRUE) 
+par(mfrow = c(1,7))
+
+
+concentrations <- data.frame(concentrations = c(conc(fitness$TotalSteps), 
+                    conc(fitness$TotalDistance), 
+                    conc(fitness$TotalActiveMinutes), 
+                    conc(fitness$SedentaryMinutes), 
+                    conc(fitness$Calories)), variable = c("TotalSteps", "TotalDistance", "TotalActiveMinutes", "SedentaryMinutes", "Calories"))
+
+concentrations <- concentrations[order(concentrations$concentrations, decreasing = TRUE), ]
+concentration_max <- concentrations$variable[1] # TotalDistance hat größte Ungleichheit 
+concentration_min <- concentrations$variable[5] # SedentaryMinutes hat die geringste Ungleichheit 
