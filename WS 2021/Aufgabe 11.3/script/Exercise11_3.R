4c1e4310a7efd2eb0592f7a9a85df828eca28789
fdiederi <fdiederi@uni-muenster.de>
2022-12-26 13:13:19 +0100

    Aufgabe 11.3 added

---
+++ WS 2021/Aufgabe 11.3/script/Exercise11_3.R
@@ -0,0 +1,9 @@
+# Aufgabe 11.3 
+setwd("/Users/felixdiederichs/Desktop/Wirtschaftsinformatik/5. Semester/Daten und Wahrscheinlichkeiten/R/WS 2021/Aufgabe 11.3")
+
+# Aufgabe 11.3 a) 
+covid <- read.csv(file = "data/covid_19_daily_reports_01-15-2022.csv", header = TRUE, sep = ";")
+world <- read.csv(file = "data/World_Countries.csv", header = TRUE, sep = ";")
+
+
+
